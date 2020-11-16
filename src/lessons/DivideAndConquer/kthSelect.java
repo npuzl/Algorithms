f831806ce5423cdc27b3f2f4816c21a76ced373d
zl <3303705211@qq.com>
2020-11-16 15:39:09 +0800

    kthSelect final version

--- src/lessons/DivideAndConquer/kthSelect.java
+++ src/lessons/DivideAndConquer/kthSelect.java
@@ -1,39 +1,82 @@
 package lessons.DivideAndConquer;
 
-/**选择第k大(小)的元素
+import java.util.Random;
+
+/**
+ * 选择第k大(小)的元素
  * 时间复杂度O(n)
+ *
  * @author zl
- * @date 2020/09/17
- * @version 1.0
+ * @version final version
+ * @date 2020/11/16
  */
-public class kthSelect {
+public class kthSelect<T extends Comparable> {
 
-    public static int kthSelectLargest(int []list,int k){
-        if(k>list.length){
-            return -1;
+    kthSelect(T[] num, int k) {
+        T[] list = num.clone();
+        System.out.println(kthSelectHelper(list, k, 0, num.length - 1));
+
+
+    }
+
+    /**
+     * @param list
+     * @param k     第K大
+     * @param left  左区间
+     * @param right 右区间 都是闭区间
+     * @return
+     */
+    public T kthSelectHelper(T[] list, int k, int left, int right) {
+        if (k > list.length || k < 1) return null;
+        int index = kthSelectFromList(list, k, left, right);
+        if (index == k - 1) {
+            return list[index];
         }
-        int left=0,right = list.length-1;
-        while (left < right){
-            int mid=(left + right)/2;
-            if(k>(mid-left+1)){ //3 4大
-                left=mid;
-            }else {
-                right=mid;
+        if (index < k - 1)
+            return kthSelectHelper(list, k, index + 1, right);
+        else
+            return kthSelectHelper(list, k, left, index - 1);
+    }
+
+    /**
+     * 这个函数用于在left和right随机找一个，返回其在整个数组中的索引
+     * 这里是左边大右边小
+     *
+     * @param list
+     * @param k
+     * @param l
+     * @param r
+     * @return
+     */
+    public int kthSelectFromList(T[] list, int k, int l, int r) {
+        int left = l;
+        int right = r;
+        int pos = new Random().nextInt(r - l + 1) + l;
+        T pivot = list[pos];//以第pos+1个数为枢轴
+        int pit = pos;//坑的索引
+        while (left < right) {
+            while (list[left].compareTo(pivot) > 0 && left < right) {
+                left++;
             }
-
+            if (left <= right) {
+                list[pit] = list[left];
+                pit = left;
+            }
+            while (list[right].compareTo(pivot) < 0 && left < right) {
+                right--;
+            }
+            if (left <= right) {
+                list[pit] = list[right];
+                pit = right;
+            }
         }
-        return 0;
+        list[pit] = pivot;
+        return pit;
     }
 
-    public static void largestHelper(int[] list){
 
-    }
-    public static int kthSelectSmallest(int []list,int k){
-        return 0;
-    }
     public static void main(String[] args) {
-        int []list = new int[]{1,2,3,4,5,6,7,8,10,};
-        System.out.println(kthSelectLargest(list,3));
-        System.out.println(kthSelectSmallest(list, 3));
+        Integer[] list = new Integer[]{3, 8, 5, 6, 7, 4, 9, 2, 11, 0};
+        new kthSelect<Integer>(list, 12);
     }
 }
---
+++ src/summer/leetcode/CountOfSmallerNumbersAfterSelf.java
@@ -0,0 +1,20 @@
+package summer.leetcode;
+
+import java.util.List;
+
+public class CountOfSmallerNumbersAfterSelf<T extends Comparable> {
+
+    public List<T> countSmaller(int[] nums) {
+
+
+
+
+        return null;
+    }
+
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{5, 2, 6, 1};
+        System.out.println(new CountOfSmallerNumbersAfterSelf<Integer>().countSmaller(nums));
+    }
+}
